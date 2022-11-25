8803582e97d86e519c07aefe5706d35f0220b2cf
niida_shuto <k021g1267@g.neec.ac.jp>
2022-11-25 16:03:05 +0900

    00_00完成

--- MT2_BaseProject/main.cpp
+++ MT2_BaseProject/main.cpp
@@ -4,6 +4,25 @@
 //関数プロトタイプ宣言
 void DrawAxis3D(const float length);
 
+//球の描画
+int DrawSphere3D(const Vector3& CenterPos, const float r, const int DivNum,
+	const unsigned int DifColor, const unsigned int SpcColor, const int FillFlag);
+//関数プロトタイプ宣言
+//int DrawCircle(Vector2 vec, int r, unsigned int color);
+
+//線分の描画
+int DrawLine3D(const Vector3& Pos1, const Vector3& Pos2, const unsigned int color);
+
+int DrawCone3D(const Vector3& TopPos, const Vector3& BottomPos, const float r,
+	const int DivNum, const unsigned int DivColor, const unsigned int SpcColor, const int FillFlag);
+
+//カメラの位置と姿勢の設定
+int SetCameraPositionAndTargetAndUpVec(
+	const Vector3& cameraPosition,//カメラの位置
+	const Vector3& cameraTarget,//カメラの注視点
+	const Vector3& cameraUp
+);
+
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
 {
@@ -28,9 +47,9 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	SetCameraNearFar(1.0f, 1000.0f);//カメラの有効範囲を設定
 	SetCameraScreenCenter(WIN_WIDTH / 2.0f, WIN_HEIGHT / 2.0f);//画面の中心をカメラの中心に合わせる
 	SetCameraPositionAndTargetAndUpVec(
-		VGet(0.0f,0.0f,-120.0f),//カメラの位置
-		VGet(0.0f, 0.0f, 0.0f),//カメラの注視点
-		VGet(0.0f, 1.0f, 0.0f)//カメラの上の向き
+		Vector3(0.0f,0.0f,-120.0f),//カメラの位置
+		Vector3(0.0f, 0.0f, 0.0f),//カメラの注視点
+		Vector3(0.0f, 1.0f, 0.0f)//カメラの上の向き
 	);
 
 	//時間計測に必要なデータ
@@ -119,5 +138,43 @@ void DrawAxis3D(const float length)
 	DrawLine3D(VGet(0, -length, 0), VGet(0, +length, 0), GetColor(0, 255, 0));//y軸
 	DrawLine3D(VGet(0, 0, -length), VGet(0, 0, +length), GetColor(0, 0, 255));//z軸
 	
+}
+
+int DrawSphere3D(const Vector3& CenterPos, const float r, const int DivNum,
+	const unsigned int DifColor, const unsigned int SpcColor, const int FillFlag)
+{
+	VECTOR centerPos = { CenterPos.x,CenterPos.y,CenterPos.z };//構造体 初期化子リスト
+
+	return DrawSphere3D(centerPos, r, DivNum, DifColor, SpcColor, FillFlag);//DxLib関数の呼び出し
+}
+int DrawCone3D(const Vector3& TopPos, const Vector3& BottomPos, const float r,
+	const int DivNum, const unsigned int DifColor, const unsigned int SpcColor, const int FillFlag)
+{
+	VECTOR topPos = { TopPos.x,TopPos.y,TopPos.z };
+	VECTOR bottomPos = { BottomPos.x,BottomPos.y,BottomPos.z };
+
+	return DrawCone3D(topPos, bottomPos, r, DivNum, DifColor, SpcColor, FillFlag);
+}
+//線分の描画
+//DXLib=> int DrawLine3D(VECTOR Pos1, VECTOR Pos2, unsigned int color);
+int DrawLine3D(const Vector3& Pos1, const Vector3& Pos2, const unsigned int color)
+{
+	VECTOR p1 = { Pos1.x,Pos1.y,Pos1.z };//構造体 初期化子リスト
+	VECTOR p2 = { Pos2.x,Pos2.y,Pos2.z };//構造体 初期化子リスト
+
+	return DrawLine3D(p1, p2, color);
+}
+//カメラの位置と姿勢の設定
+int SetCameraPositionAndTargetAndUpVec(
+	const Vector3& cameraPosition,//カメラの位置
+	const Vector3& cameraTarget,//カメラの注視点
+	const Vector3& cameraUp
+)
+{
+	VECTOR position = { cameraPosition.x,cameraPosition.y,cameraPosition.z };
+	VECTOR target = { cameraTarget.x,cameraTarget.y,cameraTarget.z };
+	VECTOR up = { cameraUp.x,cameraUp.y,cameraUp.z };
+
+	return SetCameraPositionAndTargetAndUpVec(position, target, up);
 }
 
